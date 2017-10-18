688919f638c773c76c129122bcb440708fbb37b2
xiaohaomao <xiaohaomao16@gmail.com>
2017-10-18 22:58:17 +0800

    Add files via upload

--- src/draft_model.py
+++ src/draft_model.py
@@ -6,8 +6,9 @@ import pandas as pd
 import os
 import random
 
-import tensorflow as tf
 
+
+import tensorflow as tf
 # add the layer #
 def add_layer(inputs, wei,bias,activation_function=None):
 
@@ -36,11 +37,10 @@ def dnn_model(X):
     dnn_encoded=add_layer(dnn_hl_2,weight_dnn_3_trained,bias_dnn_3_trained,activation_function=tf.nn.sigmoid)
 
     dnn_classifi_1=add_layer(dnn_encoded,wei_dnn_1,bias_dnn_1,activation_function=tf.nn.sigmoid)
-    dnn_classifi_2=add_layer(dnn_classifi_1,wei_dnn_2,bias_dnn_2,activation_function=tf.nn.sigmoid)
-    prediction=add_layer(dnn_classifi_2,wei_dnn_3,bias_dnn_3,activation_function=tf.nn.sigmoid)
+    #dnn_classifi_2=add_layer(dnn_classifi_1,wei_dnn_2,bias_dnn_2,activation_function=tf.nn.sigmoid)
+    prediction=add_layer(dnn_classifi_1,wei_dnn_3,bias_dnn_3,activation_function=tf.nn.sigmoid)
 
     return prediction
-
 
 # Import data from siyuan #
 
@@ -100,9 +100,9 @@ train_step_auto = tf.train.AdagradOptimizer(0.001).minimize(loss_auto)
 
 ## the dnn classifier ##
 
-wei_dnn_1= tf.Variable(tf.random_normal([32, 128],mean=0.0, stddev=0.05))
-wei_dnn_2= tf.Variable(tf.random_normal([128, 128],mean=0.0, stddev=0.05))
-wei_dnn_3=tf.Variable(tf.random_normal([128, 94],mean=0.0, stddev=0.05))
+wei_dnn_1= tf.Variable(tf.random_normal([32, 128]))
+wei_dnn_2= tf.Variable(tf.random_normal([128, 128]))
+wei_dnn_3=tf.Variable(tf.random_normal([128, 94]))
 
 bias_dnn_1=tf.Variable(tf.constant(0.1,shape=[128]))
 bias_dnn_2=tf.Variable(tf.constant(0.1,shape=[128]))
@@ -153,36 +153,29 @@ with tf.Session() as sess:
     #print("test_y",test_y[[0,1]])
     #print("test_y",len(test_y[[0,1]]))
     #print("test_x", len(test_x[0]))
-
-
-
-    batch_size=256;
+    batch_size = 256;
 
     print('....autoecoder Training....')
-    for i in range(30001):
+    for i in range(15001):
 
         random_number = random.sample(range(0, len(train_x)), batch_size)
 
-        sample_x=train_x[random_number]
+        sample_x = train_x[random_number]
         sample_y = train_y[random_number]
 
         train_step_auto.run({x: sample_x, y_: sample_y})
-
 
         if i % 1000 == 0:
             train_loss = loss_auto.eval({x: sample_x, y_: sample_y})
 
             print('  step, loss = %6d: %6.3f' % (i, train_loss))
 
-
         if i % 1000 == 0:
             test_data = {x: test_x, y_: test_y}
 
             print('loss (test) = ', loss_auto.eval(test_data))
-    print("the decode", decoded.eval({x: test_x[[0,1]], y_: test_y[[0,1]]}))
-    print("the x...",test_x[[0,1]])
-
-
+    print("the decode", decoded.eval({x: test_x[[0, 1]], y_: test_y[[0, 1]]}))
+    print("the x...", test_x[[0, 1]])
 
     print(".....deep neural network classifier training.... ")
 
@@ -197,33 +190,37 @@ with tf.Session() as sess:
         random_number = random.sample(range(0, len(train_x)), batch_size)
         sample_x = train_x[random_number]
         sample_y = train_y[random_number]
+        sess.run(train_step_dnn,feed_dict={x: sample_x, y_: sample_y, weight_dnn_1_trained: w1, weight_dnn_2_trained: w2,
+                 weight_dnn_3_trained: w3, bias_dnn_1_trained: b1, bias_dnn_2_trained: b2, bias_dnn_3_trained: b3})
 
-       ## connected the trained weights with a full connection network ##
-        if i_dnn % 1000 == 0:
-            train_loss_dnn = dnn_cross_entropy.eval({x:sample_x , y_: sample_y,weight_dnn_1_trained:w1,weight_dnn_2_trained:w2,
-                           weight_dnn_3_trained:w3,bias_dnn_1_trained:b1,bias_dnn_2_trained:b2,bias_dnn_3_trained:b3})
+        ## connected the trained weights with a full connection network ##
+        if i_dnn % 500 == 0:
+            train_loss_dnn = dnn_cross_entropy.eval(
+                {x: sample_x, y_: sample_y, weight_dnn_1_trained: w1, weight_dnn_2_trained: w2,
+                 weight_dnn_3_trained: w3, bias_dnn_1_trained: b1, bias_dnn_2_trained: b2, bias_dnn_3_trained: b3})
 
             print('  dnn_step, dnn_loss = %6d: %6.3f' % (i_dnn, train_loss_dnn))
-            #print("the prediction",prediction.eval({x:sample_x , y_: sample_y,weight_dnn_1_trained:w1,weight_dnn_2_trained:w2,
-                           #weight_dnn_3_trained:w3,bias_dnn_1_trained:b1,bias_dnn_2_trained:b2,bias_dnn_3_trained:b3}))
-            #print("the sample_y", sample_y)
+            # print("the prediction",prediction.eval({x:sample_x , y_: sample_y,weight_dnn_1_trained:w1,weight_dnn_2_trained:w2,
+            # weight_dnn_3_trained:w3,bias_dnn_1_trained:b1,bias_dnn_2_trained:b2,bias_dnn_3_trained:b3}))
+            # print("the sample_y", sample_y)
 
-
-        if i_dnn % 1000 == 0:
-
+        if i_dnn % 500 == 0:
             correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y_, 1))
             accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-            accuracy_train = sess.run(accuracy, feed_dict={x: sample_x, y_: sample_y,weight_dnn_1_trained:w1,weight_dnn_2_trained:w2,
-                           weight_dnn_3_trained:w3,bias_dnn_1_trained:b1,bias_dnn_2_trained:b2,bias_dnn_3_trained:b3})
+            accuracy_train = sess.run(accuracy, feed_dict={x: sample_x, y_: sample_y, weight_dnn_1_trained: w1,
+                                                           weight_dnn_2_trained: w2,
+                                                           weight_dnn_3_trained: w3, bias_dnn_1_trained: b1,
+                                                           bias_dnn_2_trained: b2, bias_dnn_3_trained: b3})
             error_train = 1 - accuracy_train
 
-            accuracy_test = sess.run(accuracy, feed_dict={x: test_x, y_: test_y,weight_dnn_1_trained:w1,weight_dnn_2_trained:w2,
-                           weight_dnn_3_trained:w3,bias_dnn_1_trained:b1,bias_dnn_2_trained:b2,bias_dnn_3_trained:b3})
+            accuracy_test = sess.run(accuracy, feed_dict={x: test_x, y_: test_y, weight_dnn_1_trained: w1,
+                                                          weight_dnn_2_trained: w2,
+                                                          weight_dnn_3_trained: w3, bias_dnn_1_trained: b1,
+                                                          bias_dnn_2_trained: b2, bias_dnn_3_trained: b3})
             error_test = 1 - accuracy_test
-
 
             print("....dnn_train_accuracy....", accuracy_train)
             print("....dnn_train_error...", error_train)
 
-            print("....dnn_test_accuracy....",accuracy_test)
+            print("....dnn_test_accuracy....", accuracy_test)
